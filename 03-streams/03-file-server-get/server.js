cdf2af5cd664b4b219d672301032f6d6d6650e13
Dmitrii Kravchenko <krav4enkodm@gmail.com>
2022-09-20 22:59:26 +0300

    build: add types

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,24 +1,22 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
-
-const server = new http.Server();
-
-server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
-
-  switch (req.method) {
-    case 'GET':
-
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const path_1 = __importDefault(require("path"));
+const server = new http_1.default.Server();
+server.on("request", (req, res) => {
+    var _a;
+    const url = new URL((_a = req.url) !== null && _a !== void 0 ? _a : "", `http://${req.headers.host}`);
+    const pathname = url.pathname.slice(1);
+    const filepath = path_1.default.join(__dirname, "files", pathname);
+    switch (req.method) {
+        case "GET":
+            break;
+        default:
+            res.statusCode = 501;
+            res.end("Not implemented");
+    }
 });
-
-module.exports = server;
+exports.default = server;
