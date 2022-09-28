28a90af894229af78273d738154c9ade7fedd98c
Dmitrii Kravchenko <krav4enkodm@gmail.com>
2022-09-28 19:25:25 +0300

    refactor(project): compiled files

--- 03-streams/04-file-server-post/LimitExceededError.js
+++ 03-streams/04-file-server-post/LimitExceededError.js
@@ -1,11 +1,11 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', {value: true});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class LimitExceededError extends Error {
-  constructor() {
-    super('Limit has been exceeded.');
-    this.code = 'LIMIT_EXCEEDED';
-    this.name = this.constructor.name;
-    Error.captureStackTrace(this, this.constructor);
-  }
+    constructor() {
+        super('Limit has been exceeded.');
+        this.code = 'LIMIT_EXCEEDED';
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
 }
 exports.default = LimitExceededError;
