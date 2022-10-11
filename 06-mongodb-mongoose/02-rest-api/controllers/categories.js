5301bcd01069af525e2747bd51855e65d40786ef
Dmitrii Kravchenko <krav4enkodm@gmail.com>
2022-10-11 21:46:32 +0300

    feat(06): add product and category endpoints

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,8 @@
+const Category = require('../models/Category.js');
+const mapCategory = require('../mappers/category.js');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find({});
+
+  ctx.body = {categories: categories.map(mapCategory)};
 };
